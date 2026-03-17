8bcac994fec4e79773e3d00a2fafd3efa8be98ee
Zakariaisaq-555 <phenomenalform89@gmail.com>
2026-03-17 17:50:23 +0300

    Initial-class-setup
    initial class setup for the assignment

--- src/Student.java
+++ src/Student.java
@@ -1,1 +1,7 @@
-
+public class Student {
+     private String name; 
+     private int studentId;
+     public Student(String name, int studentId) {
+         this.name = name;
+         this.studentId = studentId;
+}
